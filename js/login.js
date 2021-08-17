0283f9b29bfcf160e8b43e29ea7b99327babd93e
alexlarrosa <alex-larrosa33@hotmail.com>
2021-08-17 01:53:34 -0300

    new config

--- js/login.js
+++ js/login.js
@@ -3,7 +3,17 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("sendBTN").onclick= function(e){
-        window.location.href="home.html";
+        let userField = document.getElementById("user");
+        let passField = document.getElementById("password");
+        let completField = true;
+
+        if(userField.value == "" || passField.value == "" ){
+            completField = false;
+            alert("Ingresa los datos para continuar")
+        }else{
+            window.location.href="home.html";
+        }
+        
         
     }
 
--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var productsArray = [];
+
+function showProductList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+        {
+
+             htmlContentToAppend += `
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name + " - " + product.currency + " " + product.cost +`</h4>
+                            <small class="text-muted"> cantidad de vendidos ` + product.soldCount + ` </small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                    </div>
+                </div>
+            </a>
+           `
+        }
+
+        document.getElementById("listaDeProductos").innerHTML = htmlContentToAppend;
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultOBJ){
+    if(resultOBJ.status === "ok"){
+        productsArray = resultOBJ.data;
 
-});
+        showProductList(productsArray);
+    }
+
+    }
+)});
